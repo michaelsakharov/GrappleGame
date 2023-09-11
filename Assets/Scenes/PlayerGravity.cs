a56015c3a395aacb919af1b2da817c9f315d6bca
Wulferis <acer1171090@gmail.com>
2023-09-11 20:47:19 +1000

    Create PlayerGravity.cs

---
+++ Assets/Scenes/PlayerGravity.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class PlayerGravity : IPlayerInteractor
+{
+    public float gravityScale = 1.0f;
+
+    public override void Interact(PlayerController player, ContactPoint2D hit)
+    {
+        player.gravityScalers.Add(this, gravityScale);
+    }
+
+    public override void TriggerInteract(PlayerController player)
+    {
+        player.gravityScalers.Add(this, gravityScale);
+    }
+
+    public override void OnLeave(PlayerController player)
+    {
+        player.gravityScalers.Remove(this);
+    }
+}
