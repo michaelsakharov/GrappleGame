755584d577e2b189e3be4fdeef3a287cfa3f7efd
Wulferis <acer1171090@gmail.com>
2023-10-19 17:41:23 +1100

    Fixed an error with the Props list in editor

--- Assets/Scripts/LevelEditor/PropButton.cs
+++ Assets/Scripts/LevelEditor/PropButton.cs
@@ -9,6 +9,7 @@ public class PropButton : Button
     protected override void Start()
     {
         base.Start();
+        if (LevelEditor.Instance == null) return;
         LevelEditor.SnapTo(LevelEditor.Instance.props, LevelEditor.Instance.propBrowser, this.transform as RectTransform);
         onClick.AddListener(() => SelectState.Instantiate(prefab));
     }
